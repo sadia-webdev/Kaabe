b8c0c5bfce4a5e4784d7acd8ed5a5f4d873089fd
sadiya959 <sacdamamoud@gmail.com>
2026-09-14 15:12:47 +0300

    fixed issue

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,6 +14,9 @@ export const auth = betterAuth({
     provider: "pg",
     schema,
   }),
+
+  trustedOrigins: ["http://localhost:3000", "https://kaabe-three.vercel.app"],
+  
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -24,5 +27,3 @@ export const auth = betterAuth({
   },
   plugins: [nextCookies()],
 });
-
-
